e83ba652ac4935fd815248c52322502024f71401
rmacmill <rmacmillian@gmail.com>
2015-03-08 21:34:11 -0500

    HW 1 completed

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,17 +9,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet weak var history: UILabel!
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var numberHasADecimalPoint = false
+    
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        if let digit = sender.currentTitle{
+            if( numberHasADecimalPoint && digit == "."){
+                // do nothing; additional decimal point is not allowed
+            }else {
+                if (digit == "."){
+                    numberHasADecimalPoint = true
+                }
+                if userIsInTheMiddleOfTypingANumber {
+                    display.text = display.text! + digit
+                } else {
+                    display.text = digit
+                    userIsInTheMiddleOfTypingANumber = true
+                }
+            }
+            println("digit = \(digit)")
+            addToHistory(digit)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        addToHistory(operation)
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "π":
+                displayValue =  M_PI
+                enter()
+        default: break
+        }
+        
     }
-
-
+    
+    @IBAction func clear(sender: UIButton) {
+        display.text = "0"
+        history.text = " "
+        operandStack.removeAll()
+        history.text = " "
+        println("operandStack = \(operandStack)")
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func addToHistory(value: String) {
+        if let oldText = history.text {
+            history.text = oldText + " " + value
+        }else {
+            history.text = value
+        }
+    }
+    
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        numberHasADecimalPoint = false
+        operandStack.append(displayValue)
+        println("operandStack = \(operandStack)")
+        
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
-
--- CalculatorBrain/CalculatorBrain/ViewController.swift
+++ CalculatorBrain/CalculatorBrain/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var numberHasADecimalPoint = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        if let digit = sender.currentTitle{
+            if( numberHasADecimalPoint && digit == "."){
+                // do nothing additional decimal point is not allowed
+            }else {
+                if (digit == "."){
+                    numberHasADecimalPoint = true
+                }
+                if userIsInTheMiddleOfTypingANumber {
+                    display.text = display.text! + digit
+                } else {
+                    display.text = digit
+                    userIsInTheMiddleOfTypingANumber = true
+                }
+            }
+            println("digit = \(digit)")
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+        
     }
-
-
+    
+    
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
 }
 
